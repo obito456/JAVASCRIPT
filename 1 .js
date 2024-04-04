18fa9dec0e3a1c9266d913bcbfa517e7c24cbf43
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-04 18:02:08 +0530

    Create 1 .js

---
+++ 1 .js
@@ -0,0 +1,10 @@
+ console.log("HELLO"); 
+ console.log("vijay"); 
+
+window.alert('this is an alert!');
+window.alert('do not touch!');
+
+document.getElementById("myh1").textContent="hello";
+document.getElementById("myp1").textContent="we will win";
+
+//comment danger
