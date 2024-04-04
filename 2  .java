23988ed095e1b3561d9821fe6a9d273b8ff082d7
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-04 18:14:35 +0530

    Create 2  .java

---
+++ 2  .java
@@ -0,0 +1,10 @@
+let age=22;
+let fname="vijay";
+let married=true;
+
+console.log(typeof age); 
+console.log(`you are ${age}`); 
+console.log(typeof fname); 
+console.log(`you are ${fname}`); 
+console.log(typeof married); 
+console.log(`you are ${married}`); 
