b98582bdbb5182c30bfc334e0119dc7772218c39
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-04 18:21:19 +0530

    Create 3   .js

---
+++ 3   .js
@@ -0,0 +1,7 @@
+let fname="vijay";
+let age=22;
+let student=true;
+
+document.getElementById("p1").textContent=`your name is ${fname}`;
+document.getElementById("p2").textContent=`your age is ${age}`;
+document.getElementById("p3").textContent=`your are a ${student}`;
