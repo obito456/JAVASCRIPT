608e41eac5adf70f643ad054d5e65588dd40c221
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-04 18:26:07 +0530

    Create 4  .js

---
+++ 4  .js
@@ -0,0 +1,18 @@
+let students=50;
+
+students=60;
+console.log(students);
+students=students-10;
+console.log(students);
+students=students+5;
+console.log(students);
+students+=5;
+console.log(students);
+students--;
+console.log(students);
+students=students*2;
+console.log(students);
+students=students**2;
+console.log(students);
+students=students%5;
+console.log(students);
