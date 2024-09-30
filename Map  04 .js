8e8217c5fcc6c54cc9bfeb470f9a4a630ed51034
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-09-30 10:49:11 +0530

    Create Map  04 .js

---
+++ Map  04 .js
@@ -0,0 +1,9 @@
+let myMap = new Map();
+
+myMap.set('name', 'Vijay');
+myMap.set(1, 'one');
+myMap.set({x: 10}, 'object as key');
+
+console.log(myMap.size);
+myMap.clear();
+console.log(myMap.size);
