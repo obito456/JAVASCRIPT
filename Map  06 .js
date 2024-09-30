80e11d4e435978288e70ab191e43407b1918e476
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-09-30 10:55:53 +0530

    Create Map  06 .js

---
+++ Map  06 .js
@@ -0,0 +1,12 @@
+let myMap = new Map();
+
+myMap.set('name', 'Vijay');
+myMap.set('age', 24);
+
+for(let a of myMap.keys()){
+    console.log(a);
+}
+
+for(let b of myMap.values()){
+    console.log(b);
+}
