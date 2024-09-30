a106afc50af4e4ea3d3593b3bdcc1ebb0eac2e02
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-09-30 16:34:59 +0530

    Create Atlasaaian  01 .js

---
+++ Atlasaaian  01 .js
@@ -0,0 +1,12 @@
+let x={
+    flag:1,
+    toString:function(){
+        return this.flag++;
+    }
+}
+if(x==1 && x==2 && x==3){
+    console.log("Good Job");
+}
+else{
+    console.log("Bad Job");
+}
