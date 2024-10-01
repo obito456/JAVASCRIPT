7fcde2a991e4ec10d83a0b6e065e52c94ee9bb80
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-01 16:08:31 +0530

    Create Object  P3 .js

---
+++ Object  P3 .js
@@ -0,0 +1,12 @@
+const profile={
+    Name:'vijay',
+    Age:24,
+    Bikes:{
+        bike1:'shine',
+        bike2:'destiny',
+        bike3:'activa'
+    }
+};
+console.log(profile.Bikes.bike1);
+console.log(profile.Bikes["bike2"]);
+console.log(profile["Bikes"]["bike3"]);
