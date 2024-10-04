4d89171d252dc53cab3187295369f4a8e0ad5e9f
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-04 17:08:53 +0530

    Update String  01 .js

--- String  01 .js
+++ String  01 .js
@@ -2,8 +2,10 @@ let a="vijay";
 let b='vijay kumar "cars"';
 let c="ajays 'pens'";
 let d=new String("hello");
+let e=`good`;
 
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
+console.log(e);
