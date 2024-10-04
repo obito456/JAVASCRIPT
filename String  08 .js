4465ff9f382654c66ab413cb835403a6298d32cf
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-04 17:42:01 +0530

    Create String  08 .js

---
+++ String  08 .js
@@ -0,0 +1,9 @@
+let a="vijay";
+let b=" hi how are you vijay, hi vijay";
+
+console.log(a.repeat(2));
+console.log(a.repeat(3));
+console.log(a.repeat(4));
+console.log(b.replace("vijay","ramesh"));
+console.log(b.replace(/VIJAY/i,"ramesh"));
+console.log(b.replace(/vijay/g,"ramesh"));
