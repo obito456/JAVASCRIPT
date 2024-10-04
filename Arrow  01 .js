d771f5bc0096e9482cfbf190f4797b80cdeab677
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-04 16:55:10 +0530

    Create Arrow  01 .js

---
+++ Arrow  01 .js
@@ -0,0 +1,9 @@
+let myFunction = (a, b) => a * b;
+let hello=()=>{
+    return "Hello Vijay";
+}
+let greet=()=> "Good Morning";
+
+console.log(myFunction(4, 5));
+console.log(hello());
+console.log(greet());
