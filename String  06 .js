36230e9e18bc382ebb097bab65eb0467bc74cabb
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-04 17:29:38 +0530

    Create String  06 .js

---
+++ String  06 .js
@@ -0,0 +1,10 @@
+let a="      Vijay";
+let b="kumar  ";
+
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+console.log(a.concat(b));
+console.log(a.concat(" ",b));
+console.log(a.trim());
+console.log(a.trimStart());
+console.log(b.trimEnd(),b.length);
